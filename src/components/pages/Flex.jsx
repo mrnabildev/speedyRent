a7ce474ddf0d67de0e83ace533be675e9c5aa1d0
mrnabildev <mr.nabil.dev@gmail.com>
2026-02-21 11:19:38 +0600

    feat(flex): add flex component

---
+++ src/components/pages/Flex.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Flex = ({className, children}) => {
+  return (
+    <div className={`${className} flex items-center`}>{children}</div>
+  )
+}
+
+export default Flex
